0388da9c6df72f3c55b146a53f45df2c24f05197
Adrian Dinu <adrian.dinu@outlook.com>
2021-12-16 20:11:45 +0200

    Lesson 13 - Memoization.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,33 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript Masterclass</h1>
-  <h2>Recursion</h2>
+  <h2>Memoization</h2>
   <p>(Check the console!)</p>
 `;
 
-// 3 x 2 x 1 = 6
-// 5 x 4 x 3 x 2 x 1 = 120
-
-console.log("------factorial imperative-------");
-
-const factorialImperative = (n) => {
-  let result = 1;
-  for (let count = n; count > 1; count--) {
-    result = result * count;
-  }
-  return result;
-};
-
-console.log(factorialImperative(3)); // 6
-console.log(factorialImperative(6)); // 720
-console.log(factorialImperative(9)); // 362880
-
-console.log("------factorial declarative-------");
-
-const factorialDeclarative = (n) => {
+const factorial = (n) => {
   if (n > 1) 
-    return n * factorialDeclarative(n - 1);
+    return n * factorial(n - 1);
 
   return 1;
 };
 
-console.log(factorialDeclarative(3)); // 6
-console.log(factorialDeclarative(6)); // 720
-console.log(factorialDeclarative(9)); // 362880
+const memoize = (fn) => {
+  const cache = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    // console.log(key); // [9, {"name":"Todd Motto"}], [6]
+    if (key in cache) {
+      return cache[key];
+    } else {
+      cache[key] = fn.apply(null, args);
+      return cache[key];
+    }
+  };
+};
+
+const memoizedFactorial = memoize(factorial);
+
+console.log(memoizedFactorial(9, { name: "Todd Motto" })); // 362880
+console.log(memoizedFactorial(6)); // 720
+console.log(memoizedFactorial(9, { name: "Todd Motto" })); // 362880
