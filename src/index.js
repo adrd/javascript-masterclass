6df81ad961413d138200bbf86cf0dad74431d5a3
Adrian Dinu <adrian.dinu@outlook.com>
2022-01-20 15:46:15 +0200

    Lesson 16 - Object Literals and Factory Functions.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript Masterclass</h1>
-  <h2>Object Literals and Encapsulation</h2>
+  <h2>Object Literals and Factory Functions</h2>
   <p>(Check the console!)</p>
 `;
 
-const cart = {
-  items: [],
-  add(item) {
-    this.items.push(item);
-  },
-  remove(id) {
-    const index = this.items.findIndex((obj) => obj.id === id);
-    this.items.splice(index, 1);
-  },
+const createCart = (items = []) => {
+  return {
+    items,
+    add(item) {
+      this.items.push(item);
+    },
+    remove(id) {
+      const index = this.items.findIndex((obj) => obj.id === id);
+      this.items.splice(index, 1);
+    },
+  };
 };
 
+const cart = createCart();
 const hotDog = { id: "🌭", name: "Posh Dog", price: 399 };
 
 cart.add(hotDog);
 console.log(cart);
 
-setTimeout(() => {
-  cart.remove("🌭");
-  console.log(cart);
-}, 2500);
+cart.items.splice(0, 1); // remove one item from index 0, it's not recommended to do this
+console.log(cart);
