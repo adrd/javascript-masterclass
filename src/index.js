15cd0c83ff5239bd8a05f2dfec643e920fecb4e5
Adrian Dinu <adrian.dinu@outlook.com>
2022-01-20 15:29:37 +0200

    Lesson 15 - Object Literals and Encapsulation.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript Masterclass</h1>
-  <h2>Procedural Programming</h2>
+  <h2>Object Literals and Encapsulation</h2>
   <p>(Check the console!)</p>
 `;
 
-const cart = [];
-
-const addToCart = (item) => {
-  cart.push(item);
-};
-
-const removeFromCart = (id) => {
-  const index = cart.findIndex((obj) => obj.id === id);
-  cart.splice(index, 1);
+const cart = {
+  items: [],
+  add(item) {
+    this.items.push(item);
+  },
+  remove(id) {
+    const index = this.items.findIndex((obj) => obj.id === id);
+    this.items.splice(index, 1);
+  },
 };
 
 const hotDog = { id: "🌭", name: "Posh Dog", price: 399 };
 
-addToCart(hotDog);
+cart.add(hotDog);
 console.log(cart);
 
 setTimeout(() => {
-  removeFromCart("🌭");
+  cart.remove("🌭");
   console.log(cart);
 }, 2500);
