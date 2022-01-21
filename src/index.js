6718f10cdf492084c5e0279ed9e851b11a0e8717
Adrian Dinu <adrian.dinu@outlook.com>
2022-01-21 16:46:54 +0200

    Lesson 22 - Setters and Getters.

--- src/index.js
+++ src/index.js
@@ -3,33 +3,40 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript Masterclass</h1>
-  <h2>Private and Static Class Members</h2>
+  <h2>Setters and Getters</h2>
   <p>(Check the console!)</p>
 `;
 
 class Cart {
-  static name = "My Cart";
   #items;
 
   constructor(items = []) {
+    this.value = items;
+  }
+
+  set value(items) {
     this.#items = Object.freeze(items);
   }
 
+  get value() {
+    return Object.freeze(this.#items);
+  }
+
+  get count() {
+    return this.value.length;
+  }
+
   add(item) {
-    const state = [...this.#items, item];
-    this.#items = Object.freeze(state);
+    this.value = [...this.value, item];
   }
 
   remove(id) {
-    const state = this.#items.filter((item) => item.id !== id);
-    this.#items = Object.freeze(state);
+    this.value = this.value.filter((item) => item.id !== id);
   }
 }
-
-console.log(Cart.name); // Array.isArray()
 
 const cart = new Cart();
 const hotDog = { id: "🌭", name: "Posh Dog", price: 399 };
 
-cart.add(hotDog); // [].filter()
-console.log(cart);
+cart.add(hotDog);
+console.log(cart.value);
