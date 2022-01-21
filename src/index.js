0582000e2ba8b4300cc8e8260b3a7a052de4502d
Adrian Dinu <adrian.dinu@outlook.com>
2022-01-21 16:22:47 +0200

    Lesson 21 - Private and Static Class Members.

--- src/index.js
+++ src/index.js
@@ -3,34 +3,33 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript Masterclass</h1>
-  <h2>Classes and Members</h2>
+  <h2>Private and Static Class Members</h2>
   <p>(Check the console!)</p>
 `;
 
 class Cart {
-  items;
+  static name = "My Cart";
+  #items;
 
   constructor(items = []) {
-    this.items = Object.freeze(items);
+    this.#items = Object.freeze(items);
   }
 
   add(item) {
-    const state = [...this.items, item];
-    this.items = Object.freeze(state);
+    const state = [...this.#items, item];
+    this.#items = Object.freeze(state);
   }
 
   remove(id) {
-    const state = this.items.filter((item) => item.id !== id);
-    this.items = Object.freeze(state);
+    const state = this.#items.filter((item) => item.id !== id);
+    this.#items = Object.freeze(state);
   }
 }
+
+console.log(Cart.name); // Array.isArray()
 
 const cart = new Cart();
 const hotDog = { id: "🌭", name: "Posh Dog", price: 399 };
 
-cart.add(hotDog);
+cart.add(hotDog); // [].filter()
 console.log(cart);
-
-console.log(cart.constructor);
-console.log(cart.__proto__);
-console.log(cart instanceof Cart); // true
